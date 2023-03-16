d3217b351eaefcccfa57efd5c8966c3669f5f345
Meenu <meenu.balagopal451@gmail.com>
2023-03-16 23:05:32 +0530

    Added converter functionality

--- App.tsx
+++ App.tsx
@@ -1,57 +1,78 @@
 import {View, Text, TextInput, Button, StyleSheet} from 'react-native';
 import React, {useState} from 'react';
 import {Picker} from '@react-native-picker/picker';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import CountryPicker from 'react-native-country-picker-modal';
 
 const App = () => {
-  const [selectedLanguage, setSelectedLanguage] = useState();
+  const [currency, setCurrency] = useState(0);
+  const [result, setResult] = useState(0);
+  const [From, setFrom] = useState();
+  const [To, setTo] = useState();
+  const [switchA, setSwitchA] = useState(false);
+
+  const swichButton = () => {
+    setTo(From);
+    setFrom(To);
+    // setSwitchA(!switchA);
+  };
+
+  const convert = () => {
+    if (To === 'USD' && From === 'INR') {
+      if (From !== 0) {
+        setResult(currency / 80);
+      }
+    } else {
+      setResult(currency * 80);
+    }
+  };
+  console.log('To', To, 'From', From);
+  console.log('result', result, 'currency', currency);
+
   return (
     <View style={{paddingHorizontal: 22}}>
       <View style={{justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{marginTop: 40}}>Exchange Rate</Text>
-        <Text style={styles.cur}>$300</Text>
+        <Text style={styles.exchange}>Exchange Rate</Text>
+        <Text style={styles.cur}>
+          {result}
+          {To === 'USD' ? '$' : ' INR'}
+        </Text>
       </View>
 
       <View>
-        <Text>Amount</Text>
-        <TextInput style={styles.amount} placeholder="Enter the Amount" />
+        <Text style={{fontWeight: 'bold'}}>Amount</Text>
+        <TextInput
+          style={styles.amount}
+          placeholder="Enter the Amount"
+          onChangeText={txt => setCurrency(txt)}
+        />
         <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-          <View>
-            <Text>From</Text>
-            <View
-              style={{
-                height: 40,
-                width: 100,
-                borderWidth: 1,
-                borderRadius: 6,
-                justifyContent: 'center',
-              }}>
-              <Picker
-                selectedValue={selectedLanguage}
-                onValueChange={(itemValue, itemIndex) =>
-                  setSelectedLanguage(itemValue)
-                }>
-                <Picker.Item label="Java" value="java" />
-                <Picker.Item label="JavaScript" value="js" />
-              </Picker>
+          {
+            <View>
+              <Text style={{fontWeight: 'bold'}}>From</Text>
+              <View style={styles.picker}>
+                <Picker
+                  selectedValue={From}
+                  onValueChange={(itemValue, itemIndex) => setFrom(itemValue)}>
+                  <Picker.Item label="INR" value="INR" />
+                  <Picker.Item label="USD" value="USD" />
+                </Picker>
+              </View>
             </View>
+          }
+          {/* <Icon name="arrows-rotate" size={30} color="#900" /> */}
+          <View style={{marginTop: 29, width: 35}}>
+            <Ionicons onPress={swichButton} name="repeat-outline" size={21} />
           </View>
           <View>
-            <Text>To</Text>
-            <View
-              style={{
-                height: 40,
-                width: 100,
-                borderWidth: 1,
-                borderRadius: 6,
-                justifyContent: 'center',
-              }}>
+            <Text style={{fontWeight: 'bold'}}>To</Text>
+            <View style={styles.picker}>
               <Picker
-                selectedValue={selectedLanguage}
-                onValueChange={(itemValue, itemIndex) =>
-                  setSelectedLanguage(itemValue)
-                }>
-                <Picker.Item label="Java" value="java" />
-                <Picker.Item label="JavaScript" value="js" />
+                selectedValue={To}
+                onValueChange={(itemValue, itemIndex) => setTo(itemValue)}>
+                <Picker.Item label="INR" value="INR" />
+                <Picker.Item label="USD" value="USD" />
               </Picker>
             </View>
           </View>
@@ -61,6 +82,7 @@ const App = () => {
         <Button
           title="Convert"
           color="#841584"
+          onPress={convert}
           accessibilityLabel="Learn more about this purple button"
         />
       </View>
@@ -79,9 +101,17 @@ const styles = StyleSheet.create({
   cur: {color: 'black', fontSize: 20, fontWeight: 'bold', marginVertical: 10},
   amount: {
     height: 40,
-    width: '95%',
-    borderColor: 'gray',
+    width: '99%',
+    borderColor: 'black',
     borderWidth: 1,
     marginBottom: 20,
   },
+  picker: {
+    height: 40,
+    width: 150,
+    borderWidth: 1,
+    borderRadius: 6,
+    justifyContent: 'center',
+  },
+  exchange: {marginTop: 40, fontWeight: 'bold', fontSize: 15},
 });
